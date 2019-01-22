06dde6f6c29449e651f36ffd2109235f1b6d54dd
Szymon Woźniak <swozniak6@gmail.com>
2019-01-22 18:39:46 +0100

    add kalman filter

--- main.py
+++ main.py
@@ -32,53 +32,85 @@ def num_iters(T, h):
 
 def simulate(T):
     u = 1
+    a = 1
     accelnoise = 0.2
     timestep = 0.1
     iters = num_iters(T, timestep)
-    x0 = [[0], [0]]
+    x0 = np.array([[0], [0]])
     A = np.array([[1, 0.1], [0, 1]])
-    B = np.array([[0.005], [0.1]])
-    C = [[1, 0]]
-
-    state_history = np.zeros((iters, 2, 1))
-    state_history[0] = x0
-    measurement_history = np.zeros(iters)
+    B = np.array([[1/2 * a * timestep ** 2], [0.1]])
+    C = np.array([[1, 0]])
     measnoise = 10
+
+    Sz = measnoise ** 2
+    Sw = accelnoise ** 2 * np.array([[timestep ** 4 / 4, timestep ** 3 / 2], [timestep ** 3 / 2, timestep ** 2]])
+    P = Sw
+
+    xhat = x0
+    state_history = np.zeros((iters, 2, 1))
+    kalman_history = np.zeros((iters, 2, 1))
+    state_history[0] = x0
+    kalman_history[0] = xhat
+    measurement_history = np.zeros(iters)
     for i in range(iters - 1):
-        # a = 3 * np.sin(timestep * i) + 2
+        # u = 3 * np.sin(timestep * i) + 2
+        # print(u)
         process_noise = accelnoise * np.array([[1/2 * timestep**2 * np.random.normal()], [timestep * np.random.normal()]])
         measure_noise = np.random.normal() * measnoise
         x = A@(state_history[i]) + B*u + process_noise
         y = C @ x + measure_noise
+
+        xhat = A @ xhat + B * u
+        inn = y - C @ xhat
+        s = C @ P @ C.T + Sz
+        K = A @ P @ C.T @ np.linalg.inv(s)
+        xhat = xhat + K @ inn
+        kalman_history[i+1] = xhat
         state_history[i+1] = x
         measurement_history[i+1] = y
-    return state_history, measurement_history, np.arange(0, T, timestep)
+    return state_history, measurement_history, np.arange(0, T, timestep), kalman_history
 
 
 if __name__ == '__main__':
-    history, measurement_history, times = simulate(10)
+    history, measurement_history, times, kalman = simulate(20)
     errors = measurement_history - np.squeeze(history[:, 0, :])
+    error_after = np.squeeze(kalman[:, 0, :]) - np.squeeze(history[:, 0, :])
 
     cols = 1
-    rows = 2
+    rows = 3
     f, axes = plt.subplots(rows, cols)
 
     name, value = ['p', history[:, 0, :]]
     axes[0].plot(times, value, label=name)
     name, value = ['measured_p', measurement_history]
     axes[0].plot(times, value, label=name)
+    name, value = ['kalman_p', kalman[:, 0, :]]
+    axes[0].plot(times, value, label=name)
 
     name, value = ['v', history[:, 1, :]]
     axes[1].plot(times, value, label=name)
+    name, value = ['kalman_v', kalman[:, 1, :]]
+    axes[1].plot(times, value, label=name)
+
+    name, value = ['errors', errors]
+    axes[2].plot(times, value, label=name)
+
+    name, value = ['errors_kalman', error_after]
+    axes[2].plot(times, value, label=name)
 
     axes[0].set_xlabel('t')
     axes[0].set_ylabel('stan')
     axes[0].legend()
 
     axes[1].set_xlabel('t')
-    axes[1].set_ylabel('stan')
+    axes[1].set_ylabel('predkosc')
     axes[1].legend()
 
+    axes[2].set_xlabel('t')
+    axes[2].set_ylabel('error')
+    axes[2].legend()
+
+    print(np.sum(error_after))
     plt.show()
 
 
